17f800eccb9078661b4442f5369ecf39aa503333
Aryan <aryan_rawat@persistent.com>
2023-02-07 17:36:10 +0530

    completion

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const userRouter = require('./api/user/user.router');
+const cors = require('cors');
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRouter); 
 
--- frontend/my-app/src/components/pages/Register/Main.tsx
+++ frontend/my-app/src/components/pages/Register/Main.tsx
@@ -1,19 +1,67 @@
 import React from "react";
-import { useState } from 'react';
+import { useState } from "react";
+import { useNavigate} from 'react-router-dom';
+
 
 const Main = () => {
-    const [inputs, setInputs] = useState<any>({});
+  const [inputs, setInputs] = useState<any>({});
+  const navigate = useNavigate();
 
-    const handleChange = (event : any) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs((values: any) => ({...values, [name]: value}))
-      }
-    
-      const handleSubmit = (event : any) => {
-        event.preventDefault();
-        console.log(inputs);
-      };
+  const handleChange = (event: any) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values: any) => ({ ...values, [name]: value }));
+  };
+
+//   var jsonData = {
+//     "restaurant_name": inputs.restaurant_name,
+//     "contact_name": inputs.contact_name,
+//     "pincode": inputs.pincode,
+//     "location": inputs.location,
+//     "website": inputs.website,
+//     "phone_number": inputs.phone_number,
+//     "average_daily_transactions": inputs.average_daily_transactions,
+//   };
+
+async function postFormDataAsJson({ url, formData }:any) {
+	const plainFormData = Object.fromEntries(formData.entries());
+	const formDataJsonString = JSON.stringify(plainFormData);
+
+	const fetchOptions = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: formDataJsonString,
+	};
+
+	const response = await fetch(url, fetchOptions);
+
+	if (!response.ok) {
+		const errorMessage = await response.text();
+		throw new Error(errorMessage);
+	}
+
+	return response.json();
+}
+
+  const handleSubmit = async(event: any) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const url = "http://localhost:5000/api/users";
+    try {
+		const formData = new FormData(form);
+		const responseData = await postFormDataAsJson({ url, formData });
+
+		console.log({ responseData });
+
+	} catch (error) {
+		console.error(error);
+	}
+    alert("Form Submitted Successfully");
+    navigate('/');
+  };
 
   return (
     <div className="main bgimg">
@@ -24,7 +72,12 @@ const Main = () => {
               <label>Restaurant Name</label>
             </div>
             <div>
-              <input type="text" name="restaurant_name" value={inputs.restaurant_name || ""} onChange={handleChange}/>
+              <input
+                type="text"
+                name="restaurant_name"
+                value={inputs.restaurant_name || ""}
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className="inputbox">
@@ -32,7 +85,12 @@ const Main = () => {
               <label>Contact Name</label>
             </div>
             <div>
-              <input type="text" name="contact_name" value={inputs.contact_name || ""} onChange={handleChange}/>
+              <input
+                type="text"
+                name="contact_name"
+                value={inputs.contact_name || ""}
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className="inputbox">
@@ -40,7 +98,12 @@ const Main = () => {
               <label>Pincode</label>
             </div>
             <div>
-              <input type="number" name="pincode" value={inputs.pincode || ""} onChange={handleChange}/>
+              <input
+                type="number"
+                name="pincode"
+                value={inputs.pincode || ""}
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className="inputbox">
@@ -48,7 +111,12 @@ const Main = () => {
               <label>Location</label>
             </div>
             <div>
-              <input type="text" name="location" value={inputs.location || ""} onChange={handleChange}/>
+              <input
+                type="text"
+                name="location"
+                value={inputs.location || ""}
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className="inputbox">
@@ -56,7 +124,12 @@ const Main = () => {
               <label>Website</label>
             </div>
             <div>
-              <input type="text" name="website" value={inputs.website || ""} onChange={handleChange}/>
+              <input
+                type="text"
+                name="website"
+                value={inputs.website || ""}
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className="inputbox">
@@ -64,7 +137,12 @@ const Main = () => {
               <label>Phone no.</label>
             </div>
             <div>
-              <input type="number" name="phone" value={inputs.phone || ""} onChange={handleChange}/>
+              <input
+                type="number"
+                name="phone_number"
+                value={inputs.phone_number || ""}
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className="inputbox">
@@ -72,7 +150,12 @@ const Main = () => {
               <label>Daily average transactions</label>
             </div>
             <div>
-              <input type="number" name="dat" value={inputs.dat || ""} onChange={handleChange}/>
+              <input
+                type="number"
+                name="average_daily_transactions"
+                value={inputs.average_daily_transactions || ""}
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className="inputbox">
